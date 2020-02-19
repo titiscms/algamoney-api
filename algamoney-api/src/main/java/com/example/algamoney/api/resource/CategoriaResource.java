c48d7434a83727b13c3a2074979ef1b693220fd2
titiscms <titisbackup@gmail.com>
2020-02-19 00:22:42 -0300

    Implementando PUT para atualizar total uma pessoa

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
+import com.example.algamoney.api.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
@@ -31,6 +33,9 @@ public class CategoriaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private CategoriaService categoriaService; 
 	
 	@GetMapping
 	public List<Categoria> listar() {
@@ -54,5 +59,11 @@ public class CategoriaResource {
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
 		categoriaRepository.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaService.atualizar(codigo, categoria);
+		return ResponseEntity.ok(categoriaSalva);
 	}
 }
---
+++ algamoney-api/src/main/java/com/example/algamoney/api/service/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.example.algamoney.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.algamoney.api.model.Categoria;
+import com.example.algamoney.api.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria atualizar(Long codigo, Categoria categoria) {
+		Categoria categoriaSalva = categoriaRepository.findOne(codigo);
+		if(categoriaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(categoria, categoriaSalva, "codigo");
+		return categoriaRepository.save(categoriaSalva);
+	}
+}
