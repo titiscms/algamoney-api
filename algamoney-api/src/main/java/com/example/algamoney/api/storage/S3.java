02e642046994c9729585377a51dee1f62a3bf590
titiscms <titisbackup@gmail.com>
2020-05-01 11:10:12 -0300

    Criando entidades Cidade e Estado, criando a migration e os Repositories

--- algamoney-api/src/main/java/com/example/algamoney/api/storage/S3.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/storage/S3.java
@@ -90,7 +90,7 @@ public class S3 {
 	}
 	
 	public String configurarUrl(String objeto) {
-		return "\\\\" + property.getS3().getBucket() + ".s3.amazonaws.com/" + objeto;
+		return "https://" + property.getS3().getBucket() + ".s3.amazonaws.com/" + objeto;
 	}
 
 	private String gerarNomeUnico(String originalFilename) {
